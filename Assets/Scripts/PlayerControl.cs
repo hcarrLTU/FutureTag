913dac6b76b6179f4da0a13f0049e5f5ab1601f0
hcarrLTU <hcarr@ltu.edu>
2022-04-07 15:38:27 -0400

    Fixed merge conflicts. Added stun and run with ball animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -22,6 +22,7 @@ public class PlayerControl : MonoBehaviour
     public Camera Camera;
     public GameObject PlayerMarker;
     public GameObject GameBall;
+    public GameObject OtherPlayer;
     Animator animator;
     public static string winner;
 
@@ -35,17 +36,20 @@ public class PlayerControl : MonoBehaviour
     //private Vector3 lungePoint;
     private bool canMove;
     public bool isRunning;
+    public bool isStunned;
     public float stunCooldown = 0;
     private float verticalSpeed;
     private float horizontalSpeed;
     private float verticalInput;
     private float horizontalInput;
+    private float movementRaycastLength = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         canMove = true;
         pointsGained = 0;
+        lungeCooldown = 0;
         Ball ballScript = GameBall.GetComponent<Ball>();
         animator = gameObject.GetComponent<Animator>();
         ParticleSystem ps = GetComponent<ParticleSystem>();
@@ -73,9 +77,9 @@ public class PlayerControl : MonoBehaviour
         PlayerMarker.GetComponent<RectTransform>().anchoredPosition = markerPosition;
 
         RaycastHit hit;
-        Physics.SphereCast(Camera.transform.position, 10, this.transform.position - Camera.transform.position, out hit, 100);
-        Debug.Log("SphereCast hit: " + hit.point);
-        Debug.Log("Player at: " + this.transform.position);
+        Physics.SphereCast(Camera.transform.position, 1, this.transform.position - Camera.transform.position, out hit, 1 << 7);
+        //Debug.Log("SphereCast hit: " + hit.point);
+        //Debug.Log("Player at: " + this.transform.position);
         if (hit.transform == this.transform)
         {
             PlayerMarker.SetActive(false);
@@ -99,18 +103,34 @@ public class PlayerControl : MonoBehaviour
                 if (Input.GetKey(upKey)) // move up
                 {
                     verticalSpeed += runSpeed * Time.deltaTime;
+                    if (Physics.Raycast(this.transform.position, new Vector3(0, 0, 1), movementRaycastLength, 1 << 6 | 7))
+                    {
+                        verticalSpeed = 0;
+                    }
                 }
                 if (Input.GetKey(downKey)) // move down
                 {
                     verticalSpeed -= runSpeed * Time.deltaTime;
+                    if (Physics.Raycast(this.transform.position, new Vector3(0, 0, -1), movementRaycastLength, 1 << 6 | 7))
+                    {
+                        verticalSpeed = 0;
+                    }
                 }
                 if (Input.GetKey(leftKey)) // move left
                 {
                     horizontalSpeed -= runSpeed * Time.deltaTime;
+                    if (Physics.Raycast(this.transform.position, new Vector3(-1, 0, 0), movementRaycastLength, 1 << 6 | 7))
+                    {
+                        horizontalSpeed = 0;
+                    }
                 }
                 if (Input.GetKey(rightKey)) // move right
                 {
                     horizontalSpeed += runSpeed * Time.deltaTime;
+                    if (Physics.Raycast(this.transform.position, new Vector3(1, 0, 0), movementRaycastLength, 1 << 6 | 7))
+                    {
+                        horizontalSpeed = 0;
+                    }
                 }
                 if ((verticalSpeed != 0) && (horizontalSpeed != 0)) //makes diagonal speed equal to runSpeed
                 {
@@ -129,155 +149,188 @@ public class PlayerControl : MonoBehaviour
                     animator.SetBool("isRunning", true);
                 }
             }
-            if ((Input.GetKey(lungeKey)) && (lungeCooldown == 0) && (hasBall == false))
+            if ((Input.GetKey(lungeKey)) && (lungeCooldown == 0) && (hasBall == false))// && (!Physics.SphereCast(this.transform.position, 1, this.transform.forward, out hit, 1 << 6 | 7)))
             {
-                StartCoroutine(Lunge());
-                canMove = true;
-                lungeCooldown = 3;
+                if (!Physics.Raycast(this.transform.position, this.transform.forward, movementRaycastLength, 1 << 6 | 7))
+                {
+                    StartCoroutine(Lunge());
+                    canMove = true;
+                    lungeCooldown = 3;
+                }
+                if ((Input.GetKey(lungeKey)) && (lungeCooldown == 0) && (hasBall == false))
+                {
+                    StartCoroutine(Lunge());
+                    canMove = true;
+                    lungeCooldown = 3;
+                }
             }
             if (lungeCooldown > 0)
             {
                 lungeCooldown -= Time.deltaTime;
-                Debug.Log("Lunge cooldown: " + lungeCooldown);
+                //Debug.Log("Lunge cooldown: " + lungeCooldown);
             }
             else if (lungeCooldown < 0)
             {
                 lungeCooldown = 0;
             }
-            if (stunCooldown > 0)
+            if (isStunned == true)
             {
-                canMove = false;
-                stunCooldown -= 1;
+                StartCoroutine(Shake(1f, 1f));
+                //Debug.Log("Stun cooldown: " + stunCooldown);
+                //stunCooldown -= 1;
                 //StartCoroutine(Shake(0.01f, 0.1f));
-                Debug.Log("Stun cooldown: " + stunCooldown);
             }
-            else if (stunCooldown < 0)
-            {
-                stunCooldown = 0;
-                canMove = true;
-            }
+            //if (stunCooldown <= 0)
+            //{
+            //    isStunned = false;
+            //    stunCooldown = 0;
+            //    canMove = true;
+            //}
         }
-        newPosition.x += horizontalSpeed;
-        newPosition.z += verticalSpeed;
-        //newPosition = new Vector3(horizontalInput, 0, verticalInput);
-        this.transform.position = newPosition;
+            newPosition.x += horizontalSpeed;
+            newPosition.z += verticalSpeed;
+            //newPosition = new Vector3(horizontalInput, 0, verticalInput);
+            this.transform.position = newPosition;
 
-        if (hasBall == true)
+            if (hasBall == true)
+            {
+                animator.SetBool("hasBall", true);
+                pointsGained += Time.deltaTime; // change to deltaTime to get rid of inconsistency
+                if (pointsGained > pointsTotal)
+                {
+                    pointsGained = pointsTotal;
+                }
+                if (pointsGained == pointsTotal) // turn into coroutine?
+                {
+                    SceneManager.LoadScene("WinScreen");
+                    Debug.Log(this.name + " wins");
+                    WinText.winner = this.name;
+                    //SceneManager.LoadScene("SampleScene");
+                }
+            }
+            if (hasBall == false)
         {
-            pointsGained += Time.deltaTime; // change to deltaTime to get rid of inconsistency
-            if (pointsGained > pointsTotal)
-            {
-                pointsGained = pointsTotal;
-            }
-            if (pointsGained == pointsTotal) // turn into coroutine?
-            {
-                SceneManager.LoadScene("WinScreen");
-                Debug.Log(this.name + " wins");
-                WinText.winner = this.name;
-                //SceneManager.LoadScene("SampleScene");
-            }
+            animator.SetBool("hasBall", false);
         }
-    }
-
-    IEnumerator Lunge()
-    {
-        canMove = false;
-        isLunging = true;
-        float t = 0;
-        Vector3 startPosition = transform.position;
-
-        //particle stuff goes here
-        lungeParticleSystem.Play();
-
-        verticalSpeed = -this.transform.forward.x;
-        horizontalSpeed = this.transform.forward.z;
-        Vector3 targetPosition = this.transform.position + (new Vector3(horizontalSpeed, 0, verticalSpeed) * lungeDuration * lungeSpeed);
-
-        while (t < lungeDuration)
-        {
-            t += Time.deltaTime;
-            //this.transform.position = Vector3.Lerp(startPosition, targetPosition, 1/lungeSpeed); //old teleport
-            this.transform.position = Vector3.Lerp(startPosition, targetPosition, (t*lungeSpeed)/2);
-            Debug.Log("Time elapsed: " + t);
-            if (this.transform.position == targetPosition)
-            {
-                Debug.Log("Reached target position " + t);
-                isLunging = false;
-                canMove = true;
-                yield break;
-            }
-            yield return null;
         }
-        isLunging = false;
-        Debug.Log("Test");
-        //this.transform.position = targetPosition;
-    }
 
-    void OnCollisionEnter(Collision other)
-    {
-        if ((other.gameObject.tag == "Ball"))// && (hasBall == false))
+        IEnumerator Lunge()
         {
+            canMove = false;
+            isLunging = true;
+            float t = 0;
+            Vector3 startPosition = transform.position;
+
+            //particle stuff goes here
+            lungeParticleSystem.Play();
+
+            verticalSpeed = -this.transform.forward.x;
+            horizontalSpeed = this.transform.forward.z;
+            Vector3 targetPosition = this.transform.position + (new Vector3(horizontalSpeed, 0, verticalSpeed) * lungeDuration * lungeSpeed);
+
+            while (t < lungeDuration)
+            {
+                t += Time.deltaTime;
+                //this.transform.position = Vector3.Lerp(startPosition, targetPosition, 1/lungeSpeed); //old teleport
+                this.transform.position = Vector3.Lerp(startPosition, targetPosition, (t * lungeSpeed) / 2);
+                //Debug.Log("Time elapsed: " + t);
+                if (this.transform.position == targetPosition)
+                {
+                    //Debug.Log("Reached target position " + t);
+                    isLunging = false;
+                    canMove = true;
+                    yield break;
+                }
+                yield return null;
+            }
+            isLunging = false;
+            Debug.Log("Test");
+            //this.transform.position = targetPosition;
+        }
+
+        void OnCollisionEnter(Collision other)
+        {
+            if ((other.gameObject.tag == "Ball"))// && (hasBall == false))
+            {
                 hasBall = true;
                 Debug.Log(this.name + " grabbed stationary ball");
-        }
-        else if ((other.gameObject.tag == "Player") && (hasBall == false))
-        {
-            if (isLunging == true)
-            {
-                Ball ballScript = GameBall.GetComponent<Ball>();
-                ballScript.ballTransferring = true;
-                Debug.Log(this.name + " grabbed ball from other player");
             }
-        }
-        else if ((other.gameObject.tag == "Player") & (hasBall == true)){
-            //stunCooldown = 20;
-        }
-        //CameraScript Shake = Camera.GetComponent<CameraShake>();
-    }
+            else if ((other.gameObject.tag == "Player") && (hasBall == false))
+            {
+                if (isLunging == true)
+                {
+                    Ball ballScript = GameBall.GetComponent<Ball>();
+                    ballScript.ballTransferring = true;
 
-    public IEnumerator Shake(float duration, float magnitude)
-    {
-        Debug.Log("Player shaking");
-        Vector3 originalPosition = transform.localPosition;
-        float elapsed = 0f;
+                    PlayerControl otherPlayerScript = OtherPlayer.GetComponent<PlayerControl>();
+                    otherPlayerScript.isStunned = true;
 
-        while (elapsed < duration)
+                    Debug.Log(this.name + " grabbed ball from other player");
+                }
+            }
+            //else if ((other.gameObject.tag == "Player") & (hasBall == true)){
+            //    if (other.gameObject.GetComponent<PlayerControl>.isLunging == true)
+            //    {
+            //        stunned = true;
+            //    }
+            //}
+            else if ((other.gameObject.tag == "Player") & (hasBall == true))
+            {
+                //stunCooldown = 20;
+            }
+            //CameraScript Shake = Camera.GetComponent<CameraShake>();
+        }
+
+        public IEnumerator Shake(float duration, float magnitude)
         {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            //float y = Random.Range(-1f, 1f) * magnitude;
+            animator.SetBool("isStunned", true);
+            canMove = false;
+            Debug.Log("Player shaking");
+            Vector3 originalPosition = transform.localPosition;
+            float elapsed = 0f;
 
-            transform.position = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
-            elapsed += Time.deltaTime;
-            yield return 0;
+            while (elapsed < duration)
+            {
+                float x = Random.Range(-1f, 1f) * magnitude;
+                //float y = Random.Range(-1f, 1f) * magnitude;
+
+                transform.position = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+                elapsed += Time.deltaTime;
+                yield return 0;
+            }
+            transform.position = originalPosition;
+            isStunned = false;
+            canMove = true;
+            animator.SetBool("isStunned", false);
+            Debug.Log("Stun over, player can move");
+            yield break;
         }
-        transform.position = originalPosition;
+
+        //void OnTriggerEnter(Collider other)
+        //{
+        //    if ((other.gameObject.tag == "Player1") && (hasBall == false))
+        //    {
+        //        Ball ballScript = other.gameObject.GetComponent<Ball>();
+        //        ballScript.ballTransferring = true;
+        //        //other.gameObject.stunCooldown = 1;
+        //        Debug.Log(this.name + " grabbed ball from other player");
+        //    }
+        //}
+
+        //void Lunge2() // old method of doing lunge, only keeping around for reference
+        //{
+        //    canMove = false;
+        //    isLunging = true;
+        //    for (float t = 0; t < lungeDuration; t++)
+        //    {
+        //        //GetComponent<Rigidbody>().velocity = this.transform.forward * lungeSpeed;
+        //        verticalSpeed = -this.transform.forward.x;
+        //        horizontalSpeed = this.transform.forward.z;
+        //        this.transform.position = this.transform.position + (new Vector3(-verticalSpeed, 0, horizontalSpeed) * lungeSpeed);
+        //    }
+        //    GetComponent<Rigidbody>().velocity = this.transform.forward * 0;
+        //    lungeCooldown = 100;
+        //    isLunging = false;
+        //    canMove = true;
+        //}
     }
-
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if ((other.gameObject.tag == "Player1") && (hasBall == false))
-    //    {
-    //        Ball ballScript = other.gameObject.GetComponent<Ball>();
-    //        ballScript.ballTransferring = true;
-    //        //other.gameObject.stunCooldown = 1;
-    //        Debug.Log(this.name + " grabbed ball from other player");
-    //    }
-    //}
-
-    //void Lunge2() // old method of doing lunge, only keeping around for reference
-    //{
-    //    canMove = false;
-    //    isLunging = true;
-    //    for (float t = 0; t < lungeDuration; t++)
-    //    {
-    //        //GetComponent<Rigidbody>().velocity = this.transform.forward * lungeSpeed;
-    //        verticalSpeed = -this.transform.forward.x;
-    //        horizontalSpeed = this.transform.forward.z;
-    //        this.transform.position = this.transform.position + (new Vector3(-verticalSpeed, 0, horizontalSpeed) * lungeSpeed);
-    //    }
-    //    GetComponent<Rigidbody>().velocity = this.transform.forward * 0;
-    //    lungeCooldown = 100;
-    //    isLunging = false;
-    //    canMove = true;
-    //}
-}
